7574159b18df088ba5201766ec4bc8c4e519b953
Kelvin Kiprop <kelvin.kiprop96@gmail.com>
2026-09-10 11:23:49 +0300

    feat(shadcn): implement citation pattern

--- packages/shadcn/src/index.ts
+++ packages/shadcn/src/index.ts
@@ -1,6 +1,8 @@
 // Pattern component exports are added here as implementations land
 // (mirrors packages/mantine/src/index.ts ordering).
 
+export { Citation, CitationsList, InlineCitation } from "./components/citation";
+
 export { OpenInput } from "./components/open-input";
 
 export { ParameterControl } from "./components/parameter-control";
