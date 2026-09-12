ea61137335337129fc0cc090d1020955dacfb31d
Kelvin Kiprop <kelvin.kiprop96@gmail.com>
2026-09-12 23:15:11 +0300

    chore: increase commitlint body line limit to 200

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "header-max-length": [2, "always", 100],
+    "body-max-line-length": [2, "always", 200],
     "type-enum": [
       2,
       "always",
